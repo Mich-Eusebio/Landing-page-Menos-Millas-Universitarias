21a920e6877a02990a0d92364558422b14fda262
Michael <michaeleusebiodelorbe@gmail.com>
2026-04-29 18:12:21 -0400

    feat: implement redirect from /rifa to /comprame-un-dia (HU-005)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/rifa',
+        destination: '/comprame-un-dia',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
